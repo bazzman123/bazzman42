f1dd3d58005777299b8aa9bfcca922769d8f553b
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-26 15:12:35 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+function getNumber() {
+   var input = document.getElementById('inputNumber').value;
+   //console.log(input);
+   //alert(input);
+
+
+   displayDetails(input);
+}
+
+
+function displayDetails(userPhone) {
+   console.log(userPhone);
+   var fullDiv = document.createElement('div');
+   fullDiv.className = "flexbox-container-2"
+}
+*/
