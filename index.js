825d4fc21209ce3e80bf8f3e4a5d2fbd215b882f
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-27 22:02:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -104,8 +104,7 @@ function carsAPI(content, extra) {
     console.log(collectedData); //looooooooooooooooooooooooooooooooooooog
     let carsTempMain = [];
     let carsTemp2 = [];
-    console.log(data["data"]["vehicles"][1]["model"]);
-    if (String(data["data"]["vehicles"][1]["owner"]) == String(theLastOwner.join(" "))){console.log("JAAAAAAAAA")}
+    console.log(data["data"]["vehicles"][5]["ownerl"]);
     for (let i = 0; i < data["data"]["vehicles"].length-1; i++) {
       if (data["data"]["vehicles"][i]["owner"] == String(theLastOwner.join(" "))) {
         carsTempMain.push(data["data"]["vehicles"][i]["model"]);
