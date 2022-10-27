591ac6024b872f93390b8464b1d216460327ed54
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-27 22:33:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -111,10 +111,10 @@ function carsAPI(content, extra) {
     console.log(data["data"]["vehicles"][5]);
     console.log(data["data"]["vehicles"]);
     */
-    console.log(typeof(data["data"]["vehicles"][5]["owner"]));
-    console.log(typeof(theLastOwner.join(" ")));
+    console.log(typeof(data["data"]["vehicles"][5]["owner"]));//data["data"]["vehicles"][i]["owner"].normalize() === theLastOwner.join(" ").normalize()
+    console.log(typeof(theLastOwner.join(" ")));//string1.localeCompare(string2)
     for (let i = 0; i < data["data"]["vehicles"].length-1; i++) {
-      if (data["data"]["vehicles"][i]["owner"].normalize() === theLastOwner.join(" ").normalize()) {
+      if (data["data"]["vehicles"][5]["owner"]).localeCompare(theLastOwner.join(" "))) {
         carsTempMain.push(data["data"]["vehicles"][i]["model"]);
         //carsTempMain.push(data["data"]["vehicles"][i]["year"]);
         //carsTempMain.push(isCredit(data["data"]["vehicles"][i]["url"]));
