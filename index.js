e6bb25894186b97936f26740793f2350a53c1066
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-27 20:57:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -66,9 +66,9 @@ function getSearchHTML(link) {
     list_[list_.length-1] = list_[list_.length-1].replace(/^[0-9\s]*/g, '');
     collectedData["gatuadress/postord"] = [list_];
     var content = {"address":String(list_[0]), "city":String(list_[1])};
-    let namnTag = doc.getElementsByClassName("link-primary").href;
-    let mixList = namnTag.split(/\/|-/g);
-    carsAPI(content, mixList);
+    let namnTag = doc.getElementsByClassName("link-primary");
+    //let mixList = namnTag.split(/\/|-/g);
+    carsAPI(content);//, mixList);
   }).catch(err => console.log(err))
 };
 
