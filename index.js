c8d896a60e8c1cf230920fa027780661f6823745
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-31 16:04:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -172,15 +172,16 @@ function creditFromURL(list) {
       var doc1 = parser.parseFromString(html1, 'text/html');
       let creditBool = doc1.getElementById("data-credit").textContent;
       console.log("MOOOODEL", list1[i]["model"]);
-      carsTemp.push({"kredit": creditBool});
+      carsTemp.push({"kredit": creditBool, "model": list1[i]["model"], "year", list1[i]["year"], "owner": list1[i]["owner"]});
     }).catch(err => console.log(err))
   };
-  console.log(carsTemp);
+console.log(carsTemp);
+displayCars(carsTemp);
 };
 
 
 function displayCars(bilar1) {
-  
+  console.log("DISPLAAY", bilar1)
 };
 
 
