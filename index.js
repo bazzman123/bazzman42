0e89a26a25d68c4ad312c2707041e264d78bb427
bazzman123 <116677511+bazzman123@users.noreply.github.com>
2022-10-31 16:41:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ function creditFromURL(list) {
   let countCredit = 0;
   let totalCars = list.length;
   var listItem1 = document.createElement("p")
-  var textP = "På adressen är totalt" + String(totalCars) + "bilar registrerade, om någon av dom är kreditköpte listas dom nedanför:";
+  var textP = "På adressen är totalt " + String(totalCars) + " bilar registrerade, om någon av dom är kreditköpte listas dom nedanför:";
   var listText1 = document.createTextNode(textP);
   listItem1.appendChild(listText1);
   document.getElementById("carsTitle").appendChild(listItem1);
